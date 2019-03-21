8a96cc0bb44c8d2b1571267a9ce7fef5bf94c06a
Aby Abraham <abyabrkal@gmail.com>
2019-03-21 11:21:23 +0400

    Stateful Widgets added + Infinite Scroll

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair =WordPair.random();
+    //final wordPair =WordPair.random();
 
     return MaterialApp(
       title: 'Welcome to Flutter',
@@ -16,9 +16,76 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
   }
 }
+
+
+/*
+declaration State<RandomWords>. 
+This indicates that we’re using the generic State class 
+specialized for use with RandomWords. 
+Most of the app’s logic and state resides here—it 
+maintains the state for the RandomWords widget. 
+This class saves the generated word pairs, which grows 
+infinitely as the user scrolls, and favorite word pairs 
+(in part 2), as the user adds or removes them from the 
+list by toggling the heart icon.
+
+RandomWordsState depends on the RandomWords class. */
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context){
+    //final wordPair =WordPair.random();
+    //return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body:_buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /* 1 */ (context, i){
+          if (i.isOdd) return Divider(); /* 2 */
+
+          final index = 1~/2; /* 3 */
+          if(index >=_suggestions.length){
+            _suggestions.addAll(generateWordPairs().take(10)); /* 4 */
+          }
+
+          return _buildRow(_suggestions[index]);
+        },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+}
+
+
+/*
+Add the stateful RandomWords widget to main.dart. 
+The RandomWords widget does little else beside creating 
+its State class */
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
