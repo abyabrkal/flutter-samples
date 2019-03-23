0b6a6e017edf3b807d891ae8ede631f7bc0ee25b
Aby Abraham <abyabrkal@gmail.com>
2019-03-23 19:53:22 +0400

    SubClass State delivered.

--- interactivity/lib/main.dart
+++ interactivity/lib/main.dart
@@ -134,3 +134,46 @@ class FavoriteWidget extends StateWidget {
   _FavoriteWidget createState() => FavoriteWidgetState();
 }
 
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container (
+          padding:EdgeInsets.all(0),
+          child:IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox (
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+
+  void toggleFavorite() {
+    setState(() {
+      if(_isFavorited) {
+        _favoriteCount --;
+        _isFavorited = false;
+      } else {
+        _favoriteCount++;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  
+}
